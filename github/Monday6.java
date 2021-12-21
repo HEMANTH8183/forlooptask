7113ac9379b2b84ab591ef9a36d31238ffa63f1a
HEMANTH8183 <hemanth8183.xworkz@gmail.com>
2021-12-21 20:26:09 +0530

    21/12/21 task

---
+++ github/Monday6.java
@@ -0,0 +1,7 @@
+class Monday{
+	public static void main (String []args){
+		for(int number = 1 ; number <10 ;){
+		System.out.println(number);
+		}
+	}
+}
---
+++ github/Monday7.java
@@ -0,0 +1,9 @@
+class Monday{
+	public static void main (String []args){
+		int s=0;
+		for(int number = 1; number <=10 ; number++){
+		s=s+number;
+		System.out.println(s);
+		}
+	}
+}
